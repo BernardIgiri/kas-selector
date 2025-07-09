6ad2faff21640fb312d66b9b33a4b501268f15ba
Bernard Igiri <bernard.igiri@gmail.com>
2025-07-09 01:47:32 -0400

    Improving error messages.

--- src/error.rs
+++ src/error.rs
@@ -2,17 +2,17 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Application {
-    #[error("Initialization failed for {category} due to {cause}")]
+    #[error("Initialization failed for `{category}` due to `{cause}`")]
     FailedToInitialize {
         category: &'static str,
         cause: String,
     },
-    #[error("Failed to initialized! Data for {category} was incorrect value {value}")]
+    #[error("Incorrect `{category}` value `{value}` found.")]
     BadInitData {
         category: &'static str,
         value: String,
     },
-    #[error("Failed to save script {script_path} for activity {activity} and event {event}.")]
+    #[error("Failed to save script `{script_path}` for activity `{activity}` and event `{event}`.")]
     SaveDataError {
         activity: String,
         event: &'static str,
